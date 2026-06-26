d870394bca5ff72b7576debb89839d56f531b981
sudhanshutripathi01 <sudhanshutripathiaxis@gmail.com>
2026-06-26 10:13:30 -0700

    chanege

---
+++ userinput.py
@@ -0,0 +1,19 @@
+name = input("enter your name:")
+print("hello",name)
+
+
+#type checking 
+print(type(name))
+
+
+#practice of data type 
+age =int(input("enter your age:"))
+print("your age is :",age )
+print(type(age))
+
+#practice exercise 
+#printing data types in different ways
+name =input ("enter your name:")
+print("hello",name )
+print(f"hello {name}")
+print("hello {}".format(name))
