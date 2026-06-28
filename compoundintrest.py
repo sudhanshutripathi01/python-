822a2b16af28b8ceaff04bcfd7d9c6bc83c77d40
sudhanshutripathi01 <sudhanshutripathiaxis@gmail.com>
2026-06-28 06:48:17 -0700

    change

---
+++ compoundintrest.py
@@ -0,0 +1,6 @@
+principal=int(input("enter the principal amount "))
+rate=int(input("enter the rate of interest "))
+time=int(input("enter the time "))
+simple_interest = (principal * rate * time) / 100
+print("Simple Interest is:", simple_interest)
+    
